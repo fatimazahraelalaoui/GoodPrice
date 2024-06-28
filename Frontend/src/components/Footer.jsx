90fbe05029ada989750de95f1e553be3a84ae5d2
fatimazahraelalaoui <elalaouifatimazahra02@gmail.com>
2024-06-28 11:49:37 +0100

    adding the home page with nav bar

---
+++ Frontend/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-dark text-light text-center py-3">
+      <p>
+        &copy; 2024 <a href="#">GoodPrice</a></p>
+    </footer>
+  );
+};
+
+export default Footer;
